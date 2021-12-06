d8e78cef389edf6712bf8dbb3cb684ac43772dbf
develNerd <isaacakakpo4@gmail.com>
2021-12-06 16:37:46 +0000

    Updated Classes

--- src/main/java/github/iakakpo/customswitch/MyCustomSwitch.kt
+++ src/main/java/github/iakakpo/customswitch/MyCustomSwitch.kt
@@ -24,77 +24,75 @@ import androidx.compose.ui.unit.dp
  * @author Isaac Akakpo
  * Created on 12/6/2021 3:57 PM
  */
-open class MyCustomSwitch(){
-    enum class SwitchState {
-        IS_ENABLED,
-        NOT_ENABLED
+enum class SwitchState {
+    IS_ENABLED,
+    NOT_ENABLED
+}
+
+@Preview(showBackground = true)
+@Composable
+public fun JCSwitch() {
+
+
+    var currentState by remember { mutableStateOf(SwitchState.NOT_ENABLED) }
+    val transition = updateTransition(currentState, label = "SwitchState")
+    val color by transition.animateColor(transitionSpec = {
+        tween(200, easing = FastOutLinearInEasing)
+    }, label = "") {
+        when (it) {
+            SwitchState.IS_ENABLED ->
+                Green
+            else -> White
+        }
     }
 
-    @Preview(showBackground = true)
-    @Composable
-    public fun JCSwitch() {
 
 
-        var currentState by remember { mutableStateOf(SwitchState.NOT_ENABLED) }
-        val transition = updateTransition(currentState, label = "SwitchState")
-        val color by transition.animateColor(transitionSpec = {
-            tween(200, easing = FastOutLinearInEasing)
-        }, label = "") {
-            when (it) {
-                SwitchState.IS_ENABLED ->
-                    Green
-                else -> White
+    BoxWithConstraints(
+        modifier = Modifier
+            .width(70.dp)
+            .height(35.dp)
+            .clickable {
+                currentState = if (currentState == SwitchState.NOT_ENABLED) {
+                    SwitchState.IS_ENABLED
+                } else {
+                    SwitchState.NOT_ENABLED
+                }
+            }
+            .indication(remember {
+                MutableInteractionSource()
+            }, rememberRipple())
+            .background(color = color, shape = RoundedCornerShape(100))
+            .border(1.dp, Color.LightGray, shape = RoundedCornerShape(100)),
+        contentAlignment = Alignment.CenterStart
+    ) {
+
+        val roundCardSize = this.maxWidth / 2
+
+        val xOffset by transition.animateDp(
+            transitionSpec = {
+                tween(300, easing = FastOutLinearInEasing)
+            }, label = "xOffset"
+        ) { state ->
+            when (state) {
+                SwitchState.NOT_ENABLED -> 0.dp
+                SwitchState.IS_ENABLED -> this.maxWidth - roundCardSize
             }
         }
 
-
-
-        BoxWithConstraints(
+        Card(
             modifier = Modifier
-                .width(70.dp)
-                .height(35.dp)
-                .clickable {
-                    currentState = if (currentState == SwitchState.NOT_ENABLED) {
-                        SwitchState.IS_ENABLED
-                    } else {
-                        SwitchState.NOT_ENABLED
-                    }
-                }
-                .indication(remember {
-                    MutableInteractionSource()
-                }, rememberRipple())
-                .background(color = color, shape = RoundedCornerShape(100))
-                .border(1.dp, Color.LightGray, shape = RoundedCornerShape(100)),
-            contentAlignment = Alignment.CenterStart
+                .size(this.maxWidth / 2)
+                .offset(x = xOffset, y = 0.dp)
+                .padding(3.dp),
+            shape = RoundedCornerShape(100),
+            backgroundColor = White,
+            border = BorderStroke(0.5.dp, color = Color.LightGray)
         ) {
 
-            val roundCardSize = this.maxWidth / 2
-
-            val xOffset by transition.animateDp(
-                transitionSpec = {
-                    tween(300, easing = FastOutLinearInEasing)
-                }, label = "xOffset"
-            ) { state ->
-                when (state) {
-                    SwitchState.NOT_ENABLED -> 0.dp
-                    SwitchState.IS_ENABLED -> this.maxWidth - roundCardSize
-                }
-            }
-
-            Card(
-                modifier = Modifier
-                    .size(this.maxWidth / 2)
-                    .offset(x = xOffset, y = 0.dp)
-                    .padding(3.dp),
-                shape = RoundedCornerShape(100),
-                backgroundColor = White,
-                border = BorderStroke(0.5.dp, color = Color.LightGray)
-            ) {
-
-
-            }
 
         }
+
     }
 }
 
